f9c686d46cc3237c863275c694bc4eba267c38f3
theboi <ryanthe@Ryan-Thes-MacBook-Pro.local>
2019-11-30 22:35:00 +0800

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BrowserRouter from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
 
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
