04f42c1373369ed974efbe696f0f739b1b254bcf
theboi <ryanthe@Ryan-Thes-MacBook-Pro.local>
2019-12-04 20:44:39 +0800

    Updated - 404 Page added

---
+++ src/components/Error404/Error404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Error404.module.css'
+
+const Error404 = () => {
+  return (
+    <div className={style.error404}>
+      <img className={style.icon} src={require('../../assets/images/404Icon.png')} alt="404 Icon" />
+      <div>
+        <h1 className={style.alertHeader}>Oops! That's a 404!</h1>
+        <p className={style.alertP}>We couldn't find the page you were looking for...</p>
+      </div>
+      <Link className={style.link} to="home">Take me back!</Link>
+    </div>
+  )
+}
+
+export default Error404;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import NavFooter from '../components/NavFooter/NavFooter';
 import NavHeader from '../components/NavHeader/NavHeader';
 import Home from '../components/Home/Home';
+import Projects from '../components/Projects/Projects';
+import Error404 from '../components/Error404/Error404';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavHeader />
-        <Route path="/home" component={Home} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/" component={Error404} />
+        </Switch>
         <NavFooter />
       </div>
     </BrowserRouter>
