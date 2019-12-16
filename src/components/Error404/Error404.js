5f8bea005859556a259ebe9cc7a1ae6ccf8b25c4
Ryan The <ryanthe@Ryans-MacBook-Pro.local>
2019-12-16 17:35:57 +0800

    Update style for 404 and footer

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -10,7 +10,7 @@ const Error404 = () => {
         <h1 className={style.alertHeader}>Oops! That's a 404!</h1>
         <p className={style.alertP}>We couldn't find the page you were looking for...</p>
       </div>
-      <Link className={style.link} to="/home/">Take me back!</Link>
+      <Link className={style.link} to="/home">Take me back!</Link>
     </div>
   )
 }
