0e640f8af5444b8519f5500dae922c079e0c7a00
Ryan The <ryan.the.2006@gmail.com>
2020-02-28 11:06:26 +0800

    Add page template functionality

--- src/pages/PageTemplate/index.jsx
+++ src/pages/PageTemplate/index.jsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { PageTemplateDiv } from '../../components/Page/PageTemplateDiv';
+import style from './PageTemplate.module.css';
+
+import { PageTemplateDiv } from "../../components/Page/PageTemplateDiv";
 
 const PageTemplateC = props => {
+  
+  let pageElement;
   return (
     <PageTemplateDiv>
-      <h1>{props.display.header.title}</h1>
+      <div className={style.headerBox} style={{backgroundImage: `url(${props.display.header.image})`}}>
+        <h1 className={style.title}>{props.display.header.title}</h1>
+        <h2 className={style.subtitle}>{props.display.header.subtitle}</h2>
+        <h4 className={style.date}>{props.display.header.date}</h4>
+      </div>
+      <div className={style.bodyBox}>
+      {props.display.body.map((content, index) => {
+        if (content.type === "paragraph") {
+          pageElement = (
+            <p className={style.paragraphP}>{content.value}</p>
+          )
+        } else if (content.type === "image") {
+          pageElement = (
+            <figure className={style.imageFig}>
+              <img className={style.imageImg} src={content.value} alt={content.meta} />
+              <figcaption className={style.imageCaption}>{content.meta}</figcaption>
+            </figure>
+          )
+        } else {
+          pageElement = (
+            <p>An error has occured. Please inform the owner of this site at ryan.theodore.2006@gmail.com. Thanks! </p>
+          )
+        }
+        return (
+          <div className={style.bodyElement}>
+            {pageElement}
+          </div>
+        )
+      })}
+    </div>
     </PageTemplateDiv>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    display: state.currentDisplayPage
-  }
-}
+    display: state.currentDisplayPage,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
+const mapDispatchToProps = dispatch => {
+  return {};
+};
 
-export const PageTemplate = connect(mapStateToProps, mapDispatchToProps)(PageTemplateC)
+export const PageTemplate = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PageTemplateC);
