a0fe6d514f8286768fc635ba90812aaf5b1b3d7f
Ryan The <ryan.the.2006@gmail.com>
2020-02-29 21:22:15 +0800

    Add pin to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Controller, Scene } from "react-scrollmagic";
+import { Tween } from "react-gsap";
 import { Link } from "react-router-dom";
 
-import style from "./HomeOld.module.css";
+import style from "./Home.module.css";
 import { HomeLine } from "../../components/HomeLine";
 
 export const Home = () => {
   return (
     <div className={style.home}>
-      <section className={style.sAnim}>
+      <Controller>
+        {/* <section className={style.sAnim}>
         <div className={style.sAnimBox}>
           <HomeLine weight="w900" />
           <HomeLine weight="w800" />
@@ -32,12 +35,56 @@ export const Home = () => {
             <Link to="/portfolio/all">Portfolio</Link>
           </div>
         </div>
-      </section>
-
-      {/* <section className={style.}>
-
       </section> */}
+        <Scene indicators={true} duration={500} pin triggerHook={0} triggerElement={1}>
+          <section className={style.section1}>
+            <div>
+              {[false, false, true, true, true, true, false, false, false, null].map((value, index) => {
+                let returnString;
+                switch ((index+1)%4) {
+                  case 3: //1
+                    returnString = "student";
+                    break;
+                  case 2: //2
+                    returnString = "hobbyist";
+                    break;
+                  case 1: //3
+                    returnString = "developer";
+                    break;
+                  case 0: //4
+                    returnString = "designer";
+                    break;
+                  default:
+                    returnString = "default";
+                }
+                return (
+                    <p
+                      className={`${style[`text${((index+1)%4).toString()}`]} ${
+                        style.text
+                      }`} style={{top: `${(index)*50}px`}}
+                    >
+                      {`${returnString} ${returnString} ${returnString} `}
+                      <span className={value ? style.highlight : ''}>
+                        {`${returnString}`}{" "}
+                      </span>
+                      {`${returnString} ${returnString} ${returnString} `}
+                    </p>
+                );
+              })}
+            </div>
+          </section>
+        </Scene>
+      </Controller>
     </div>
   );
 };
 
+{
+  /* <Controller>
+          <Scene indicators={true} duration={1000} pin>
+            <Tween from={{ x: "100px" }}>
+              <div>This element gets tweened</div>
+            </Tween>
+          </Scene>
+        </Controller> */
+}
