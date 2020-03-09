a22097369dabf8dbe1b4ba3ecbf0793b73043a95
Ryan The <ryan.the.2006@gmail.com>
2020-03-09 20:30:22 +0800

    Change (") quotes to (`) backtick

--- src/assets/content/content.js
+++ src/assets/content/content.js
@@ -22,53 +22,58 @@ export const content = {
     Projects: [
         {
             header: {
-                display: "Personal Website",
-                title: "My Personal Portfolio Website",
-                subtitle: "Personal Projects",
-                date: "83/123/42",
+                display: `Personal Website`,
+                title: `My Personal Portfolio Website`,
+                subtitle: `Personal Projects`,
+                date: `83/123/42`,
                 tags: [],
-                image: "https://unsplash.it/1000/1000",
-                genre: ["code", "design"]
+                image: `https://unsplash.it/1000/1000`,
+                genre: [`code`, `design`]
             },
             // type: paragraph, image, link, button, quote, iframe
             body: [
-                { type: "paragraph", value: "My personal website" },
-                { type: "image", value: "https://unsplash.it/200", meta: "3M InnoScience Logo" },
-                { type: "link", value: "https://ryanthe.com", meta: "My Website" },
+                { type: `paragraph`, value: `My personal website` },
+                { type: `image`, value: `https://unsplash.it/200`, meta: `3M InnoScience Logo` },
+                { type: `link`, value: `https://ryanthe.com`, meta: `My Website` },
             ]
         },
         {
             header: {
-                display: "Personal Website (Old)",
-                title: "My Personal Portfolio Website (Old)",
-                subtitle: "Personal Projects",
-                date: "83/123/42",
+                display: `Personal Website (Old)`,
+                title: `My Personal Portfolio Website (Old)`,
+                subtitle: `Personal Projects`,
+                date: `83/123/42`,
                 tags: [],
-                image: "https://unsplash.it/1000/1000",
-                genre: ["code", "design"]
+                image: `https://unsplash.it/1000/1000`,
+                genre: [`code`, `design`]
             },
             // type: paragraph, image, link, button, quote, iframe
             body: [
-                { type: "paragraph", value: "This website is the website you are viewing right now." },
-                { type: "image", value: "https://unsplash.it/200", meta: "3M InnoScience Logo" },
-                { type: "link", value: "https://ryanthe.com", meta: "My Website" },
+                { type: `paragraph`, value: `This website is the website you are viewing right now.` },
+                { type: `image`, value: `https://unsplash.it/200`, meta: `3M InnoScience Logo` },
+                { type: `link`, value: `https://ryanthe.com`, meta: `My Website` },
             ]
         },
         {
             header: {
-                display: "RoboRAVE Kaga 2020",
-                title: "RoboRAVE Kaga 2020",
-                subtitle: "Robotics CCA",
-                date: "83/123/42",
+                display: `RoboRAVE Kaga 2020`,
+                title: `RoboRAVE Kaga 2020`,
+                subtitle: `Robotics CCA`,
+                date: `83/123/42`,
                 tags: [],
-                image: "https://unsplash.it/1000/1000",
-                genre: ["code", "robot"]
+                image: `https://unsplash.it/1000/1000`,
+                genre: [`code`, `robot`]
             },
             // type: paragraph, image, link, button, quote, iframe
             body: [
-                { type: "paragraph", value: "RoboRAVE Kaga 2019 is a robotics competition held in Kaga, Japan." },
-                { type: "image", value: "https://unsplash.it/200", meta: "3M InnoScience Logo" },
-                { type: "link", value: "https://ryanthe.com", meta: "My Website" },
+                { type: `paragraph`, value: `RoboRAVE Kaga 2019 is a robotics competition held in Kaga, Japan.` },
+                { type: `image`, value: `https://unsplash.it/200`, meta: `RoboRAVE Kaga Logo` },
+                { type: `link`, value: `https://www.roborave-kaga.com/`, meta: `RoboRAVE Kaga official website` },
+                { type: `link`, value: `http://www.roborave.org/`, meta: `RoboRAVE International official website` },
+                {
+                    type: `paragraph`, value: `During the end-of-year school break, a group of students from my 
+                school's, the School of Science and Technology, Singapore, Robotics Club CCA went on a learning 
+                journey to Kaga, Japan to participate in an international robotics competition.` },
             ]
         },
 
